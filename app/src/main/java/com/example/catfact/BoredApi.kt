005ac16609bc368acf7ca7284409b6d4e08fd195
ViktorBach <vmb0107@gmail.com>
2024-04-11 11:02:28 +0200

    created Bored api functionality exercise 2 and 3

--- app/src/main/java/com/example/catfact/BoredApi.kt
+++ app/src/main/java/com/example/catfact/BoredApi.kt
@@ -1,9 +1,18 @@
 package com.example.catfact
 
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface BoredApi {
     @GET("/api/activity/")
     suspend fun getBored(
+    ): Bored
+
+    @GET("/api/activity/")
+    suspend fun getBoredParticipants(@Query("participants") participants: Int,): Bored
+    @GET("/api/activity/")
+    suspend fun getBoredAccessibility(@Query("accessibility") accessibility: Double,
+                          @Query("minprice") minPrice: Double,
+                          @Query("maxprice") maxPrice: Double
     ): Bored
 }
--- app/src/main/java/com/example/catfact/app.kt
+++ app/src/main/java/com/example/catfact/app.kt
@@ -1,11 +1,52 @@
 package com.example.catfact
 
+import android.os.Build
+import androidx.annotation.RequiresExtension
 import kotlinx.coroutines.runBlocking
 
 fun main() {
+    //Exercise 1
     runBlocking {
         val instance = RetrofitInstance()
         val bored = instance.apiService.getBored()
         println(bored)
+    //Exercise 2
+        getParticipants()
+    //Exercise 3
+        getAccessibility()
+    }
+}
+
+//Exercise 2
+fun getParticipants () {
+        runBlocking {
+            println("Enter the number of participants:")
+            val participants = readlnOrNull()?.toIntOrNull() ?: 1
+
+            val instance = RetrofitInstance()
+            val bored = instance.apiService.getBoredParticipants(participants)
+            println(bored)
+        }
+}
+
+//Exercise 3
+fun getAccessibility () {
+    runBlocking {
+        println("Enter the accessibility (0.0 to 1.0):")
+        val accessibility = readln().toDouble()
+
+        println("Enter the minimum price (0 to 1):")
+        val minPrice = readln().toDouble()
+
+        println("Enter the maximum price (0 to 1):")
+        val maxPrice = readln().toDouble()
+
+        val instance = RetrofitInstance()
+        val response = instance.apiService.getBoredAccessibility(accessibility, minPrice, maxPrice)
+        if (response.activity == null) {
+            println("failure")
+        } else {
+            println(response.activity)
+        }
     }
 }
